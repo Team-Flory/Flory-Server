e89492bc0f633538de092c225cb46098b66e69ff
준써크 <junsuk8960@naver.com>
2023-10-10 17:59:25 +0900

    initial commit

---
+++ src/main/java/zerobibim/flory/FloryApplication.java
@@ -0,0 +1,13 @@
+package zerobibim.flory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zerobibim/flory/FloryApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobibim.flory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
