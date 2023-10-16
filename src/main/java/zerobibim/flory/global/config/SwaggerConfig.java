1c5ed7cf389b71b5b11d7ec3c1915c83c9a71a4d
준써크 <junsuk8960@naver.com>
2023-10-16 21:41:31 +0900

    chore: swagger-setting

---
+++ src/main/java/zerobibim/flory/global/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package zerobibim.flory.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Swagger for flory")
+                .description("Springdoc을 사용한 Swagger UI for flory")
+                .version("1.0.0");
+    }
+}
