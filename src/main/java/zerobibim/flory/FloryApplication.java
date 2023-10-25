0c8a01b00b635182f425e4e1d5ed5091eb8d85a7
준써크 <junsuk8960@naver.com>
2023-10-25 17:20:43 +0900

    chore: 불필요 코드 제거

--- src/main/java/zerobibim/flory/FloryApplication.java
+++ src/main/java/zerobibim/flory/FloryApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.TimeZone;
 
 @EnableJpaAuditing
@@ -17,7 +15,6 @@ public class FloryApplication {
 	public void started() {
 		// timezone UTC 셋팅
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-		System.out.println("현재시각 : " + LocalDateTime.now());
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FloryApplication.class, args);
