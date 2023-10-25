a234711399e1bf5d0719d1fa6075d496fda82600
준써크 <junsuk8960@naver.com>
2023-10-25 17:17:54 +0900

    chore: server time setting

--- src/main/java/zerobibim/flory/FloryApplication.java
+++ src/main/java/zerobibim/flory/FloryApplication.java
@@ -1,13 +1,24 @@
 package zerobibim.flory;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class FloryApplication {
 
+	@PostConstruct
+	public void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재시각 : " + LocalDateTime.now());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FloryApplication.class, args);
 	}
